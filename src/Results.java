ee98d277478e8c4fe0dbc84d69283661ef99d40e
Moiz Asghar <95476009+i201796@users.noreply.github.com>
2022-11-26 03:10:47 +0500

    "Dfs(Done) and bfs(in progress)"

---
+++ src/Results.java
@@ -0,0 +1,7 @@
+public class Results {
+
+    public static String DFSResult="";
+    public static String graph="";
+    public static String BFSResult="";
+
+}
---
+++ src/WriteFile.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+    public static void writeGraphToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("Graph Result.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(Results.graph);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeDFSToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("DFS Result.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(Results.DFSResult);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeBFSToFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("BFS Result.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(Results.BFSResult);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
